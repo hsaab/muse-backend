4dd6750d9677d467e5c544b083bef471992d7eae
Hassan Saab <hassansaab@Hassans-MacBook-Pro.local>
2018-08-14 19:10:00 -0700

    fixed headers

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "x-access-token, Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
