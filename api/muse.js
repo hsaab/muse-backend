ed94c36e4764869d52801e5e7fed0751980d6eba
Hassan Saab <hsaab310@gmail.com>
2019-07-06 18:29:55 -0700

    reverting to old commit when gmail worked

--- api/muse.js
+++ api/muse.js
@@ -61,6 +61,7 @@ async function resolveEmail(db) {
         await email_helper.send('concerts', user);
         db.query(`UPDATE users SET emailSent = true WHERE email = $1`, [user.email]);
         console.log("Done resolving email to ", user.email);
+        
       } catch(e) {
         db.query(`UPDATE users SET emailSent = false WHERE email = $1`, [user.email]);
         console.log("Trouble sending email", e);
