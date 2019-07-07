473a1fb27e09e6108e26a78ea11af8db3168bc12
Hassan Saab <hsaab310@gmail.com>
2019-07-06 18:45:45 -0700

    adding secure and port numbeR

--- helpers/email-helpers.js
+++ helpers/email-helpers.js
@@ -1,29 +1,31 @@
-var nodemailer = require('nodemailer');
-var Email = require('email-templates');
-var path = require('path');
+var nodemailer = require("nodemailer");
+var Email = require("email-templates");
+var path = require("path");
 
 const transport = nodemailer.createTransport({
-    auth: {
-      user: 'apollo.muse.concerts@gmail.com',
-      pass: process.env.MUSE_EMAIL_PASSWORD
-    },
-    service: "gmail",
-    host: 'smtp.gmail.com',
+  auth: {
+    user: "apollo.muse.concerts@gmail.com",
+    pass: process.env.MUSE_EMAIL_PASSWORD
+  },
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true
 });
 
 async function send(templateName, user) {
   let email = new Email({
-     message: { from: 'Apollo @ Muse' },
-     transport,
-     send: true,
-     preview: false,
-     views: {
-        root: './emails',
-        options: {
-            extension: 'hbs'
-        }
+    message: { from: "Apollo @ Muse" },
+    transport,
+    send: true,
+    preview: false,
+    views: {
+      root: "./emails",
+      options: {
+        extension: "hbs"
       }
-   });
+    }
+  });
   email.send({
     template: templateName,
     message: {
