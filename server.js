0ec216e38b7b31d0b56fef49ee60db6c9092e46e
Hassan Saab <hsaab310@gmail.com>
2019-07-06 19:02:21 -0700

    adding route to test email sending

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   const result = require("dotenv").config();
   if (result.error) {
     console.log(result.error);
@@ -7,17 +7,18 @@ if (process.env.NODE_ENV !== 'production') {
 
 let express = require("express");
 let cors = require("cors");
-let bodyParser = require('body-parser');
-let db = require('./pool.js');
-let spotify = require('./api/spotify.js');
+let bodyParser = require("body-parser");
+let db = require("./pool.js");
+let spotify = require("./api/spotify.js");
+let muse = require("./api/muse.js");
 let tm = require("./api/ticketmaster.js");
-let path = require('path');
-let helmet = require('helmet');
-let RateLimit = require('express-rate-limit');
+let path = require("path");
+let helmet = require("helmet");
+let RateLimit = require("express-rate-limit");
 
 var app = express();
 
-app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
 
 var limiter = new RateLimit({
   windowMs: 1000 * 60, // 1 minute
@@ -31,17 +32,27 @@ app.use(limiter);
 app.use(helmet());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, HEAD, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use('/spotify', spotify(db));
+app.use("/spotify", spotify(db));
+app.get("/sendemail", async function(req, res) {
+  await muse.resolveEmail(db);
+  res.json("ok");
+});
 
 var port = process.env.PORT || 3001;
 app.listen(port);
